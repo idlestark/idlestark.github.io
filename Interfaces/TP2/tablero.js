b544ecfcc2b13f7b944dde65065df196731d891a
idlestark <inaki.seb@gmail.com>
2023-10-29 15:45:59 -0300

    agregado tablero

---
+++ Interfaces/TP2/tablero.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext('2d');
+
+let canvasWidth = canvas.width;
+let canvasHeight = canvas.height;
+
+ctx.context.beginPath();
+ctx.fillRect(50, 0, 100, 100)
+ctx.stroke();
