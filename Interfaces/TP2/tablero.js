c72941cb7bf42786c0b2caa19a24e20d22d933e2
idlestark <inaki.seb@gmail.com>
2023-10-31 06:33:17 -0300

    Botones que andan, pero no se borran

--- Interfaces/TP2/tablero.js
+++ Interfaces/TP2/tablero.js
@@ -3,22 +3,9 @@
 
 var columnCount = 7;
 var rowCount = 6;
-var cellSize = 70;
+var cellSize = 60;
 var backgroundImage = new Image();
 
-// Cargar la imagen de fondo    
-backgroundImage.src = "https://mathcentral.uregina.ca/QQ/database/QQ.09.14/h/michael4.1.gif";
-
-// Dibuja el fondo una vez que la imagen cargó
-backgroundImage.onload = function() {
-    for (let y = 0; y < rowCount; y++) {
-        for (let x = 0; x < columnCount; x++) {
-            const cellX = x * cellSize;
-            const cellY = y * cellSize;
-            ctx.drawImage(backgroundImage, cellX, cellY, cellSize, cellSize);
-        }
-    }
-}
 
 const buttons = [
     { x: 50, y: 100, width: 100, height: 40, label: "Botón 1" },
@@ -26,41 +13,86 @@ const buttons = [
     { x: 50, y: 200, width: 100, height: 40, label: "Botón 3" },
 ];
 
-// Función para dibujar los botones
-function drawButtons() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#3498db";
-    ctx.font = "18px Arial";
+ // Definir una variable para rastrear si los botones están visibles
+ var botonesVisibles = true;
 
-    buttons.forEach((button) => {
-        ctx.fillRect(button.x, button.y, button.width, button.height);
-        ctx.fillStyle = "white";
-        ctx.fillText(button.label, button.x + 10, button.y + 25);
-    });
-}
+        function dibujarBotones() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            if (botonesVisibles) {
+                ctx.fillStyle = "blue";
+                ctx.fillRect(50, 50, 135, 50);
+                ctx.fillStyle = "green";
+                ctx.fillRect(200, 50, 135, 50);
+                ctx.fillStyle = "red";
+                ctx.fillRect(350, 50, 135, 50);
 
-// Función para ocultar los botones
-function hideButtons() {
-    buttons.forEach((button) => {
-        button.hidden = true;
-    });
-}
-
-// Función a ejecutar al hacer clic en cualquiera de los botones
-function onClickButton(event) {
-    const x = event.clientX - canvas.getBoundingClientRect().left;
-    const y = event.clientY - canvas.getBoundingClientRect().top;
-    buttons.forEach((button) => {
-        if (!button.hidden && x >= button.x && x <= button.x + button.width && y >= button.y && y <= button.y + button.height) {
-            hideButtons();
-            console.log("apretaste un botón")
+                ctx.fillStyle = "white";
+                ctx.font = "20px Arial";
+                ctx.fillText("4 en Línea", 69, 85);
+                ctx.fillText("5 en Línea", 220, 85);
+                ctx.fillText("6 en Línea", 370, 85);
+            }
         }
-    });
-}
 
-// Dibujar los botones iniciales
-drawButtons();
+        function clicEnBoton(event) {
+            if (botonesVisibles) {
+                var x = event.clientX - canvas.getBoundingClientRect().left;
+                var y = event.clientY - canvas.getBoundingClientRect().top;
 
-// Asignar el evento de clic al canvas
-canvas.addEventListener("click", onClickButton);
+                if (x >= 50 && x <= 150 && y >= 50 && y <= 100) {
+                    // Cargar la imagen de fondo    
+                    backgroundImage.src = "https://mathcentral.uregina.ca/QQ/database/QQ.09.14/h/michael4.1.gif";
+                    // Dibuja el fondo una vez que la imagen cargó
+                    backgroundImage.onload = function() {
+                        for (let y = 0; y < rowCount; y++) {
+                            for (let x = 0; x < columnCount; x++) {
+                                const cellX = x * cellSize;
+                                const cellY = y * cellSize;
+                                ctx.drawImage(backgroundImage, cellX, cellY, cellSize, cellSize);
+                                  }
+                             }
+                    }
+                    
+                } else if (x >= 200 && x <= 300 && y >= 50 && y <= 100) {
+                // Cargar la imagen de fondo    
+                backgroundImage.src = "https://mathcentral.uregina.ca/QQ/database/QQ.09.14/h/michael4.1.gif";
 
+                 // Dibuja el fondo una vez que la imagen cargó
+                 backgroundImage.onload = function() {
+                rowCount = 8;
+                columnCount = 7;
+                 for (let y = 0; y < rowCount; y++) {
+                    for (let x = 0; x < columnCount; x++) {
+                         const cellX = x * cellSize;
+                         const cellY = y * cellSize;
+                        ctx.drawImage(backgroundImage, cellX, cellY, cellSize, cellSize);
+                        }
+                     }
+                }
+                    
+                } else if (x >= 350 && x <= 450 && y >= 50 && y <= 100) {
+                    // Cargar la imagen de fondo    
+                backgroundImage.src = "https://mathcentral.uregina.ca/QQ/database/QQ.09.14/h/michael4.1.gif";
+
+                // Dibuja el fondo una vez que la imagen cargó
+                backgroundImage.onload = function() {
+               rowCount = 9;
+               columnCount = 11;
+                for (let y = 0; y < rowCount; y++) {
+                   for (let x = 0; x < columnCount; x++) {
+                        const cellX = x * cellSize;
+                        const cellY = y * cellSize;
+                       ctx.drawImage(backgroundImage, cellX, cellY, cellSize, cellSize);
+                       }
+                    }
+               }
+                
+                }
+            }
+        }
+
+        canvas.addEventListener("click", clicEnBoton);
+
+ canvas.addEventListener("click", clicEnBoton);
+
+ dibujarBotones();
